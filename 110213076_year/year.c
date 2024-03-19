5452e7283b796d40e76c116eb2bd1a550a916a6a
ALICE\alice <alicecheng.126@gmail.com>
2024-03-19 16:10:11 +0800

    閏年計算更正

--- 110213076_year/year.c
+++ 110213076_year/year.c
@@ -8,24 +8,16 @@ int main()
     printf("Enter a year: "); 
     scanf("%d", &year);
 
-    if (year % 4 == 0) //先除以 4 判斷
-    {
-        if (year % 100 == 0) //如果能夠被 100 整除
-            printf("%d 年不是閏年\n", year); // 則不是閏年
-        else //否則是閏年
-            printf("%d 年是閏年\n", year);
-    } //if
-    else if(year % 100 ==0)
-    {
-        if (year % 400 == 0)
-            printf("%d 年是閏年\n", year);
-        else
-            printf("%d 不是閏年\n", year);
-    } //else if
-    else if (year % 400 == 0) //如果能夠被 400 整除
-        printf("%d 年是閏年\n", year); //則是閏年
+    if (year % 4 !=0)
+        printf("%d 不是閏年\n", year); //不被4整除，則不為閏年
+    else if (year % 100 != 0)
+        printf("%d 是閏年\n", year); //被4整除，但不被100整除，則為閏年
+    else if (year % 100 == 0 && year % 400 != 0)
+        printf("%d 是閏年\n", year); //被100整除，但不被400整除，則不為閏年
+    else if (year % 400 == 0)
+        printf("%d 是閏年\n", year); //被400整除，則為閏年
     else
-        printf("%d 不是閏年\n", year); //否則不是閏年
+        printf("%d 不是閏年\n", year); //其他情況，則不為閏年
 
     system("pause");
     return 0;
